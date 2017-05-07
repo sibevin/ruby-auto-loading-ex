cf03fea3f47c86023b23d8f5fdbc9a0df6ae7516
Sibevin Wang <sibevin@gmail.com>
2017-05-07 15:28:49 +0800

    Step4: Load file according to the given constant

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,5 @@
+class BooksController
+  def show
+    p "The books#show is called!!"
+  end
+end
--- main.rb
+++ main.rb
@@ -1,7 +1,12 @@
 class Object
   def self.const_missing(c)
-    if c == :HomesController
-      require './homes_controller'
+    if c =~ /.*Controller\Z/
+      file_name = c.to_s.gsub(/::/, '/').
+        gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+        gsub(/([a-z\d])([A-Z])/,'\1_\2').
+        tr("-", "_").
+        downcase
+      require "./app/controllers/#{file_name}.rb"
       const_get(c)
     else
       super
@@ -11,3 +16,9 @@ end
 
 homes_ctrl = HomesController.new
 homes_ctrl.show
+
+books_ctrl = BooksController.new
+books_ctrl.show
+
+invalid_ctrl = InvaildCtrl.new
+invalid_ctrl.show
