b6e31b6bd9cb7766cb409a7fa8a79c8da19c1369
Sibevin Wang <sibevin@gmail.com>
2017-05-07 15:19:24 +0800

    Step3: Auto-load file when the constant is missing

--- main.rb
+++ main.rb
@@ -1,3 +1,13 @@
-require './homes_controller'
+class Object
+  def self.const_missing(c)
+    if c == :HomesController
+      require './homes_controller'
+      const_get(c)
+    else
+      super
+    end
+  end
+end
+
 homes_ctrl = HomesController.new
 homes_ctrl.show
