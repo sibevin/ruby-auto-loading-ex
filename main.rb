7026bd3f4a2f34f2942ba183394c8eddfb2013b7
Sibevin Wang <sibevin@gmail.com>
2017-05-09 17:55:11 +0800

    Step6: Prevent infinite calls when no constant is found after reload

--- main.rb
+++ main.rb
@@ -2,14 +2,18 @@ $LOAD_PATH << File.join(File.dirname(__FILE__), "app", "controllers")
 
 class Object
   def self.const_missing(c)
+    super if @is_reloaded
     if c =~ /.*Controller\Z/
+      @is_reloaded = true
       file_name = c.to_s.gsub(/::/, '/').
         gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
         gsub(/([a-z\d])([A-Z])/,'\1_\2').
         tr("-", "_").
         downcase
       require file_name
-      const_get(c)
+      result = const_get(c)
+      @is_reloaded = false
+      return result
     else
       super
     end
